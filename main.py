51b1c93d22f9900661ba24b7a7ef3f22da4709ea
lehlohonolomothibe <lehlohonolomothibe207@gmail.com>
2026-04-28 19:58:24 +0200

    added login endpoint

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "API running"}
+
+from fastapi import HTTPException
+
+fake_user = {
+    "username": "admin",
+    "password": "1234"
+}
+
+@app.post("/login")
+def login(username: str, password: str):
+    if username == fake_user["username"] and password == fake_user["password"]:
+        return {"message": "login success"}
+    raise HTTPException(status_code=401, detail="invalid credentials")
